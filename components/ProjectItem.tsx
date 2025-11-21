698c7ac1e7ad792f0a37263a702613fbcce64f65
Yongjun Cho <cyjun0304@naver.com>
2025-11-22 03:23:17 +0900

    feat: update project and publications

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -6,11 +6,14 @@ interface ProjectProps {
 const ProjectItem = ({ project, index }: ProjectProps): JSX.Element => {
   return (
     <div className="shadow-lg mb-8 mx-auto lg:w-11/12 lg:flex lg:flex-row lg:h-auto">
-      <img
-        className="rounded-tr-lg rounded-tl-lg h-68 w-full lg:h-auto lg:w-6/12 lg:rounded-bl-lg lg:rounded-tr-none"
-        src={project.img}
-        alt=""
-      />
+      <div className="w-full lg:w-6/12 bg-gray-100 flex items-center justify-center rounded-tr-lg rounded-tl-lg lg:rounded-bl-lg lg:rounded-tr-none overflow-hidden">
+        <img
+          className="w-full h-auto object-contain py-4 px-4"
+          src={project.img}
+          alt={project.title}
+          style={{ maxHeight: '400px' }}
+        />
+      </div>
       <div className="w-full bg-gray-50 p-8 rounded-bl-lg rounded-br-lg lg:rounded-bl-none lg:rounded-tr-lg">
         <h2 className="text-gray-700 font-semibold">{project.title}</h2>
         <p className="text-sm text-gray-500 mt-4">{project.description}</p>
