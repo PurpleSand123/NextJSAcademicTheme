3996bf8b5d15b402ef70cb5e17c38a26898453b9
Yongjun Cho <cyjun0304@naver.com>
2025-11-22 02:15:16 +0900

    feat: update supporting box

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,16 @@
 import personalInfo from './data/personalInfo.json';
 import SupportingBox from './SupportingBox';
 
-
 const Footer = (): JSX.Element => {
-    const iconStyle = "hover:bg-gray-300 dark:hover:bg-gray-600 p-1 rounded-md";
+  const iconStyle = 'hover:bg-gray-300 dark:hover:bg-gray-600 p-1 rounded-md';
 
-    return (
-        <footer>
-            <div className="mb-9 pb-0 pl-6 max-w-4xl flex items-center justify-between text-sm opacity-60 m-auto pt-20 pr-5">
-                <p> &copy; {new Date().getFullYear()}. {personalInfo.copyright}</p>
-                <SupportingBox></SupportingBox>
-            </div>
-            
-            <p className="text-center text-sm opacity-40 dark:opacity-50 pb-4 pt-4">
-                Built with <i>Next.js, TailwindCSS</i>. {' '}
-            </p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p className="text-center text-sm opacity-40 dark:opacity-50 pb-4 pt-4">
+        Built with <i>Next.js, TailwindCSS</i>.{' '}
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
