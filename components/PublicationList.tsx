d294cdb801bc4c1694a0399254cf728e3367fc5e
Yongjun Cho <cyjun0304@naver.com>
2025-11-25 21:19:13 +0900

    feat: add paper author type

--- components/PublicationList.tsx
+++ components/PublicationList.tsx
@@ -5,6 +5,9 @@ const PublicationList = (): JSX.Element => {
   return (
     <section className="grid w-full" id="publications">
       <h2 className="text-xl font-bold mt-12 mb-4">Publications</h2>
+      <p className="text-sm text-black dark:text-white mb-4">
+        † denotes equal contribution, * denotes corresponding author
+      </p>
       <div>
         {data.map((publication, index) => (
           <PublicationItem publication={publication} index={index} key={index} />
